d4c301b4f4c66deb574797904d88e2730394c423
FedeCaste9 <federicocastelluccio31@gmail.com>
2022-01-04 21:33:04 -0300

    Array de libros en otro archivo

---
+++ ejercicios/ejercico1/libros.js
@@ -0,0 +1,45 @@
+const nakamas = [{
+    nombre: "El visitante",
+    autor: "Stephen King",
+    nacautor: "Estadounidense",
+    paginas: 592
+}, {
+    nombre: "Fundación",
+    autor: "Isaac Asimov",
+    nacautor: "	Estadounidense",
+    paginas: 230
+}, {
+    nombre: "Shingeki no Kyojin, Capitulos 1, 3, 127",
+    autor: "Hajime Isayama",
+    nacautor: "Japonés",
+    paginas: 120
+}, {
+    nombre: "Sandman, Capitulos 5, 10, 12",
+    autor: "Neil Gaiman",
+    nacautor: "Inglés",
+    paginas: 105
+}, {
+    nombre: "Eragon",
+    autor: "Chistopher Paolini",
+    nacautor: "Estadounidense",
+    paginas: 544,
+    saga: "Eragon"
+}, {
+    nombre: "Eldest",
+    autor: "Chistopher Paolini",
+    nacautor: "Estadounidense",
+    paginas: 704,
+    saga: "Eragon"
+}, {
+    nombre: "Brisignr",
+    autor: "Chistopher Paolini",
+    nacautor: "Estadounidense",
+    paginas: 700,
+    saga: "Eragon"
+}, {
+    nombre: "Legado",
+    autor: "Chistopher Paolini",
+    nacautor: "Estadounidense",
+    paginas: 811,
+    saga: "Eragon"
+}]
